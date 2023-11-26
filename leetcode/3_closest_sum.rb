e8e88607817455a855e25721e7473a1976c06f9a
RGarvizu <rgarvizu@waverleysoftware.com>
2023-11-26 17:42:15 -0400

    Sliding window implementation.

--- leetcode/3_closest_sum.rb
+++ leetcode/3_closest_sum.rb
@@ -1,11 +1,26 @@
-def three_sum_closest(nums, target, min_distance = Float::INFINITY)
+def three_sum_closest(nums, target)
+  min_distance = Float::INFINITY
   result = Float::INFINITY
-  sums = nums.combination(3).map { |c| c.sum }.each { |s|
-    distance = (s - target).abs
-    if distance < min_distance
-      min_distance = distance
-      result = s
+  nums.sort!
+
+  (0..nums.length - 3).each do |i|
+    left = i + 1
+    right = nums.length - 1
+    while left < right
+      sum = nums[i] + nums[left] + nums[right]
+      distance = (sum - target).abs
+      if distance < min_distance
+        min_distance = distance
+        result = sum
+      end
+      if sum == target
+        return sum
+      elsif sum < target
+        left += 1
+      else
+        right -= 1
+      end
     end
-  }
+  end
   result
 end
